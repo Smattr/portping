10f19a8b548fe908ea08025ed6b2e20685930e27
Matthew Fernandez <matthew.fernandez@gmail.com>
2011-04-22 21:11:11 +1000

    C: Linux compatibility.

--- portping.c
+++ portping.c
@@ -5,22 +5,47 @@
     Creates a base TCP connection to a specified server and port to verify connectivity.
 */
 
-/* You'll need to pass the switch -lws2_32 to gcc for the Winsock components to compile */
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#ifdef _WIN32
+    /* You'll need to pass the switch -lws2_32 to gcc for the Winsock components
+     * to compile.
+     */
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #include <windows.h>
+#else
+    #include <netinet/in.h>
+    #include <netdb.h>
+#endif
 
-#include <windows.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
+
+static inline int init(void) {
+#ifdef _WIN32
+    WORD wVersionRequested;
+    WSADATA wsaData;
+
+    /* Initialise Winsock. */
+    wVersionRequested = MAKEWORD(1, 0);
+    return WSAStartup(wVersionRequested, &wsaData);
+#else
+    return 0;
+#endif
+}
+
+static inline void cleanup(void) {
+#ifdef _WIN32
+    /* Unload Winsock resources. */
+    WSACleanup();
+#endif
+}
 
 int main(int argc, char **argv)
 {
     int sockfd, portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-    WORD wVersionRequested;
-    WSADATA wsaData;
     struct timeval tick;
     struct timeval tock;
     int result;
@@ -39,12 +64,9 @@ int main(int argc, char **argv)
         return 0;
     }
     
-    /* Initialise Winsock */
-    wVersionRequested = MAKEWORD(1, 0);
-    if (WSAStartup(wVersionRequested, &wsaData) != 0)
-    {
-        fprintf(stderr, "Failed to load Winsock\n");
-        return 0;
+    if (init()) {
+        perror("Error during initialisation");
+        return 1;
     }
 
     do
@@ -53,7 +75,7 @@ int main(int argc, char **argv)
         if (sockfd < 0)
         {
             perror("Error opening socket");
-            WSACleanup();
+            cleanup();
             return 0;
         }
         
@@ -61,7 +83,7 @@ int main(int argc, char **argv)
         if (server == NULL)
         {
             perror("Error: no such host");
-            WSACleanup();
+            cleanup();
             return 0;
         }
         
@@ -75,15 +97,18 @@ int main(int argc, char **argv)
         elapsed = (tock.tv_sec - tick.tv_sec) * 1000 + (tock.tv_usec - tick.tv_usec) / 1000;
         
         if (result < 0) 
-            fprintf(stderr, "Connection failed (time=%dms)\n", elapsed);
+            fprintf(stderr, "Connection failed (time=%lums)\n", elapsed);
         else
-            printf("Response from %s:%s (time=%dms)\n", argv[1], argv[2], elapsed);
+            printf("Response from %s:%s (time=%lums)\n", argv[1], argv[2], elapsed);
+#ifdef _WIN32
         closesocket(sockfd);
+#else
+        close(sockfd);
+#endif
     }
     while (argc >= 4 && strcmp(argv[3], "-t") == 0);
     
-    /* Unload Winsock resources */
-    WSACleanup();
+    cleanup();
     return 0;
 }
 
